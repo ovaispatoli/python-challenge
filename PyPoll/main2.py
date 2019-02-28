5b1f2081de46406cb8a11c260703b21c80d6a163
OvaisPatoli <ovaispatoli@gmail.com>
2019-02-28 12:12:21 -0600

    Calculated Total Votes, initialized empty lists

--- PyPoll/main2.py
+++ PyPoll/main2.py
@@ -15,11 +15,35 @@ Candidates = []
 with open(csv_path, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
-    print(csv_header)
+    #print(csv_header)
+    #loop over to read csv and assign values to predefined lists
     for row in csvreader:
         voter_ids.append(row[0])
         Counties.append(row[1])
         Candidates.append(row[2])
 
-#test print
-print(voter_ids[0],Counties[0], Candidates[0])
+#Calculate Total Votes, use set() to remove duplicate id's if they exist
+Total_Votes = len(list(set(voter_ids)))
+
+
+#Print Election results and dotted line
+#print("Election Results")
+#print("---------------------")
+
+#print Total Votes
+print(f"Total Votes: {Total_Votes}")
+
+#print dotted line
+#print("---------------------")
+
+#print percentages of votes and Total Votes for each candidate
+#print(f"Khan: {} ({})")
+#print(f"Correy: {} ({})")
+#print(f"Li: {} ({})")
+#print(f"O'Tooley: {} ({})")
+
+#print Winner
+#print(f"Winner: {}")
+
+#print dotted line
+#print("---------------------")
