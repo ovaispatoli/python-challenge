56ac1f9b7c606af365e9b432f7c78a28ddaf8544
OvaisPatoli <ovaispatoli@gmail.com>
2019-02-27 21:56:21 -0600

    removed faulty csv files and renamed main.py to main2.py in 'PyPoll'

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,4 +4,13 @@ import os
 import csv
 
 #Create path, read file
+current = os.getcwd()
+csv_path = os.path.join(current,'budget_data.csv')
+#print(csv_path)
 
+with open (csv_path,'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=' ')
+    csv_header = next(csvreader)
+    for row in csvreader:
+        print(row)
+        
