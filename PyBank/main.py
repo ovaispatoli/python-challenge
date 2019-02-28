233566de7bf8b1e1665eaafdad8fec815259a4cf
OvaisPatoli <ovaispatoli@gmail.com>
2019-02-28 14:05:05 -0600

    fixed loop to get the right values for Monthly Change

--- PyBank/main.py
+++ PyBank/main.py
@@ -22,11 +22,14 @@ with open (csv_path,'r') as csvfile:
          Profit_Losses.append(int(row[1]))
 
 #Loop over Profit_Losses
-for row in Profit_Losses[:6]:
+for row in Profit_Losses:
     if counter > 0:
+        #store the two values in value_1 and value_2 
         value_1 = Profit_Losses[(counter - 1)]
         value_2 = Profit_Losses[counter]
+        #Calculate Monthly_Change = month2 - month1
         Monthly_Change = value_2 - value_1
+        #Append to Profit_Changes list
         Profit_Changes.append(Monthly_Change)
         #add to counter
         counter = counter + 1
@@ -34,32 +37,40 @@ for row in Profit_Losses[:6]:
         #add to counter
         counter = counter + 1
 
-print(Profit_Changes)
-
 #calculate net profits by summing up values in the list
-#Net = sum(Profit_Losses)
+Net = sum(Profit_Losses)
 
 #Total Months
-#Total_Months = len(Months)
+Total_Months = len(Months)
+
+#Calculate Average Change
+Average_Change = round(sum(Profit_Changes)/len(Profit_Changes),2)
 
 #Greatest Increase/Decrease
-#Greatest_Increase = max(Profit_Losses)
-#Greatest_Decrease = min(Profit_Losses)
+Greatest_Increase = max(Profit_Losses)
+Greatest_Decrease = min(Profit_Losses)
 
 #getting the dates associated with the Greatest
-#index_1 = Profit_Losses.index(Greatest_Increase)
-#index_2 = Profit_Losses.index(Greatest_Decrease)
+index_1 = Profit_Losses.index(Greatest_Increase)
+index_2 = Profit_Losses.index(Greatest_Decrease)
 
 #getting the values from Months[]
-#Month_1 = Months[index_1]
-#Month_2 = Months[index_2]
+Month_1 = Months[index_1]
+Month_2 = Months[index_2]
 
 #Initial print statements
-#print("Financial Analysis")
-#print("------------------------")
-#Other print statements
-#print(f"Total Months: ",Total_Months)
-#print(f"Total: ${Net}")
-#print(round(sum(Changes)/len(Changes),2))
-#print(f"Greatest Increase in Profits: {Month_1} (${Greatest_Increase})")
-#print(f"Greatest Decrease in Profits: {Month_2} (${Greatest_Decrease})")
+print("Financial Analysis")
+print("------------------------")
+
+#Print Total Months
+print(f"Total Months: ",Total_Months)
+
+#Print Net Profits
+print(f"Total: ${Net}")
+
+#Print Average Change
+print(f"Average Change: ${Average_Change}")
+
+#Print Greatest Increase and Greatest Decrease
+print(f"Greatest Increase in Profits: {Month_1} (${Greatest_Increase})")
+print(f"Greatest Decrease in Profits: {Month_2} (${Greatest_Decrease})")
