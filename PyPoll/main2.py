bb80cfac8faa20ce5d4d213cde793f63a857e3cf
OvaisPatoli <ovaispatoli@gmail.com>
2019-02-28 11:09:30 -0600

    initialized empty lists, set path and opened csv

---
+++ PyPoll/main2.py
@@ -0,0 +1,24 @@
+#import modules/libraries
+import os
+import csv
+
+#Create path, read file
+current = os.getcwd()
+csv_path = os.path.join(current, 'election_data.csv')
+
+#The empty lists
+voter_ids = []
+Counties = []
+Candidates = []
+
+#open csv
+with open(csv_path, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+    print(csv_header)
+    #for row in csvfile:
+        #voter_ids.append(row[0])
+        #Counties.append(row[1])
+        #Candidates.append(row[2])
+
+#test print
